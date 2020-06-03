99996e8aa84b2f64077591ce9697faf09e81a997
aurel fontaine <ajfame11@gmail.com>
2020-06-03 23:39:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,9 @@
 class Dog
   attr_accessor :id, :name, :breed
 
-  
+  def initialize(attributes)
+    #id: nil, name:, breed:
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    self.id ||= nil
+  end
 end
